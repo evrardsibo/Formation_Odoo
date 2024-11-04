7e0b7f8643ed441f52a45515c41b9d39c7081e3b
evrardsibo <84560003+evrardsibo@users.noreply.github.com>
2024-11-04 20:44:27 +0100

    add new

--- custom/training/models/training.py
+++ custom/training/models/training.py
@@ -11,14 +11,14 @@ class Training(models.Model):
     image = fields.Image(related='supervisor_id.image_1920',string='Image')
     line_ids = fields.One2many("training.line", "training_id")
     duration = fields.Integer(string="Duration")
-    date_start = fields.Datetime(default=datetime.date.today(), string='Date', required=True) #computer
-    date_end = fields.Datetime(compute="_add_dynamic_date_end", string='End Date', required=True)
+    date_start = fields.Date(default=datetime.date.today(), string='Date', required=True) #computer
+    date_end = fields.Date(compute="_add_dynamic_date_end", string='End Date', required=True)
 
 
     @api.depends("date_start","duration")
     def _add_dynamic_date_end(self):
         for record in self:
             if record.date_start:
-                record.date_end = record.date_start + datetime.timedelta(hours=record.duration)
+                record.date_end = record.date_start + datetime.timedelta(days=record.duration)
 
 
